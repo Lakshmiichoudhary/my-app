f8c6ef6ab0e2bf5116a61aa475c9dc3f91fad62a
lakshmiichoudhary <laxmi2252001@gmail.com>
2023-11-23 13:54:11 +0530

    multiple State

--- src/ExpenseForm/ExpenseForm.js
+++ src/ExpenseForm/ExpenseForm.js
@@ -1,22 +1,35 @@
 import './ExpenseForm.css';
+import React, {useState} from 'react';
 const ExpenseForm = () => {
+    const [enteredTitle,setEnteredTitle] = useState("");
+    const [enterAmount,setEnteredAmount] = useState("");
+    const [enteredDate,setEnteredDate] = useState("");
+
     const AddNewItem = (event) => {
-        console.log(event.target.value);
-    }
+        setEnteredTitle(event.target.value)
+    };
+
+    const AmountChange = (event) => {
+        setEnteredAmount(event.target.value)
+    };
+
+    const DateChange = (event) => {
+        setEnteredDate(event.target.value)
+    };
     return(
         <>
         <div className="new_exp">
             <div className="n_e">
                 <label>Expense Title</label>
-                <input type="text" onChange = {AddNewItem} />
+                <input type="text" value={enteredTitle} onChange = {AddNewItem} />
             </div>
             <div className="e_Am">
                 <label>Expense Amount</label>
-                <input type="number" />
+                <input type="number" value={enterAmount} onChange={AmountChange} />
             </div>
             <div className="date">
                 <label>Date</label>
-                <input type="date" />
+                <input type="date" value={enteredDate} onChange={DateChange}/>
             </div>
         </div>
         <div className="action">
