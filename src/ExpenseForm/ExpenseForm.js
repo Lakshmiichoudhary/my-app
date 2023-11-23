ca882f728f4afc545f6fb70596ddfe8ee99bf43c
lakshmiichoudhary <laxmi2252001@gmail.com>
2023-11-23 14:29:38 +0530

    form submission

--- src/ExpenseForm/ExpenseForm.js
+++ src/ExpenseForm/ExpenseForm.js
@@ -16,26 +16,36 @@ const ExpenseForm = () => {
     const DateChange = (event) => {
         setEnteredDate(event.target.value)
     };
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+        const ExpData = {
+            Title : enteredTitle,
+            amount: enterAmount,
+            date: new Date(enteredDate)
+        }
+        console.log(ExpData)
+    };
     return(
-        <>
+        <form onSubmit = {submitHandler}>
         <div className="new_exp">
             <div className="n_e">
                 <label>Expense Title</label>
-                <input type="text" value={enteredTitle} onChange = {AddNewItem} />
+                <input type="text"  onChange = {AddNewItem} />
             </div>
             <div className="e_Am">
                 <label>Expense Amount</label>
-                <input type="number" value={enterAmount} onChange={AmountChange} />
+                <input type="number" onChange={AmountChange} />
             </div>
             <div className="date">
                 <label>Date</label>
-                <input type="date" value={enteredDate} onChange={DateChange}/>
+                <input type="date" onChange={DateChange}/>
             </div>
         </div>
         <div className="action">
             <button type = "submit"> submit</button>
         </div>
-        </>
+        </form>
     )
 }
 
